4c9f5e6be77f7e005f08425c1a03774647dd571c
Shiwaangee <shiwaangee@gmail.com>
2026-03-22 20:03:15 +0530

    navbar file created

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+function NavBar() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/task">Task</Link>
+            <Link to="/notes">Notes</Link>
+            <Link to="/help">Help</Link>
+        </nav>
+    );
+}
+
+export default NavBar;
