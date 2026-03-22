077af72bcbbe9cd39efd3b40ef038967f244a207
Shiwaangee <shiwaangee@gmail.com>
2026-03-22 19:41:30 +0530

    Router defined

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import react from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import Task from './pages/task';
@@ -7,13 +8,22 @@ import Help from './pages/help';
 
 function App() {
   return (
-    <div>
-      <Home />
-      <Login />
-      <Task />
-      <Notes />
-      <Help />
-    </div>
+    // <div>
+    //   <Home />
+    //   <Login />
+    //   <Task />
+    //   <Notes />
+    //   <Help />
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/task" element={<Task/>}/>
+        <Route path="/notes" element={<Notes/>}/>
+        <Route path="/help" element={<Help/>}/>
+      </Routes>
+    </Router>
   );
 }
 
