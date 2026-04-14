3ab99945051be8fc53c779d93cf4f117d0dd8e2b
Shiwaangee <shiwaangee@gmail.com>
2026-04-14 13:05:21 +0530

    welcome with username added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,19 @@
 // import react from 'react';
 import NavBar from '../navbar';
 function Home() {
+    const currentUser = localStorage.getItem("currentUser");
     return (
         <div>
             <NavBar />
-            <h1>Home page</h1>
-            <p>Welcome to the home page!</p>
+            <h1>Welcome {currentUser ? `, ${currentUser}` : ""}</h1>
+            <div>
+                <p>
+                    You can manage your tasks, create notes, and always come back to
+                    continue. If you want a guide to understand how to use the site,
+                    please go to the Help section.
+                </p>
+            </div>
+
         </div>
     );
 }
