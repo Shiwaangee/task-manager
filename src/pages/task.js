7d7878d1c6d1a5ba0d1b734511dcffcde19794b9
Shiwaangee <shiwaangee@gmail.com>
2026-04-18 16:13:17 +0530

    mark complete logic added

--- src/pages/task.js
+++ src/pages/task.js
@@ -55,6 +55,32 @@ function Task() {
         }
         
     }
+
+    function handleComplete(id){
+        const currentUser = localStorage.getItem("currentUser");
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username === currentUser) {
+                // we got the user 
+                // go to task array of user
+                // find the task set its complete
+
+                // users[i].tasks = users[i].tasks.map(eachTask => {
+                //     if(eachTask.id === id){
+                //         return {...eachTask, completed: !eachTask.completed};
+                //     }
+                //     return task;
+                // });
+                users[i].tasks =users[i].tasks.map(eachTask => (
+                    eachTask.id === id ? {...eachTask, completed: !eachTask.completed} : eachTask
+                ));
+                localStorage.setItem("users", JSON.stringify(users));
+                setDisplayTask(users[i].tasks);
+                break;
+            }
+        }
+    }
     return (
         <div>
             <NavBar />
@@ -73,6 +99,9 @@ function Task() {
                         <li key={item.id}>
                             {item.completed ? <s>{item.text}</s> : item.text}
                             <button onClick = {() => handleDelete(item.id)}> del</button>
+                            <button onClick = {() => handleComplete(item.id)}>
+                                {item.completed ? "Undo" : "Mark Complete"}
+                            </button>
                         </li>
                     ))}
                 </ul>
