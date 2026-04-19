b700ef73f5828b00dbffebff845faf17deeb752c
Shiwaangee <shiwaangee@gmail.com>
2026-04-19 22:26:09 +0530

    delete account logic added

--- src/navbar.js
+++ src/navbar.js
@@ -7,6 +7,23 @@ function NavBar() {
         alert("Successful");
         navigate("/login");
     }
+    function deleteAccount(){
+        const currentUser = localStorage.getItem("currentUser");
+        if(!currentUser){
+            alert("No user is currently logged in.");
+            return;
+        }
+        alert("Are you sure you want to delete your account? This action cannot be undone.");
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        
+
+        users = users.filter(eachUser => (
+            eachUser.username !== currentUser
+        ));
+        localStorage.setItem("users", JSON.stringify(users));
+        localStorage.removeItem("currentUser");
+        navigate("/login");
+    }
     return (
         <nav>
             <Link to="/">Home</Link>
@@ -15,6 +32,7 @@ function NavBar() {
             <Link to="/help">Help</Link>
             <Link to="/login">Login</Link>
             <button onClick = {handleLogOut}>LogOut</button>
+            <button onClick = {deleteAccount}>DelAct</button>
         </nav>
     );
 }
