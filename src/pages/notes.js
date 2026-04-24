f5c3f7f554db9e7694ffd857c061155885c1d8ba
Shiwaangee <shiwaangee@gmail.com>
2026-04-24 20:31:07 +0530

    style added

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -71,28 +71,43 @@ function Notes() {
     return (
         <div>
             <NavBar />
-            <h1>Notes</h1>
-            <p>Here you can write and organize your notes.</p>
-            <input 
-                placeholder = "Title" 
-                value = {titleName} 
-                onChange = {(e) => setTitleName(e.target.value)}
-            />
-            <textarea
-                placeholder = "Write your notes here..."
-                value = {content}
-                onChange = {(e) => setContent(e.target.value)}
-            />
-            <button onClick = {handleAddNotes}> Add Notes</button>
+            {/* <h1>Notes</h1>
+            <p>Here you can write and organize your notes.</p> */}
+            <div className = "flex flex-col mt-20 justify-center sticky top-16">
+                <div className = "flex justify-center mb-4">
+                    <input 
+                        placeholder = "Title" 
+                        value = {titleName} 
+                        onChange = {(e) => setTitleName(e.target.value)}
+                        className = "border h-8 text-center hover:ring-2 hover:ring-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-200"
+                    />
+                </div>
+                
+                <div className = "flex mb-4">
+                    <textarea
+                        placeholder = "Write your notes here..."
+                        value = {content}
+                        onChange = {(e) => setContent(e.target.value)}
+                        className = "border w-full mx-20 h-20 pl-2 hover:ring-2 hover:ring-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-200"
+                    />
+                </div>
+                <div className = "flex justify-center">
+                    <button onClick = {handleAddNotes} className = "border p-2 bg-white hover:bg-purple-300 hover:text-white active:scale-95 transition-transform duration-150 mb-4"> Add Notes</button>
+                </div>
+            </div>
+            
 
             {displayNotes.length === 0 ? (<h5>Add your first note!!!</h5>) : (
-                <ul>
+                <ul className = "flex flex-wrap gap-4 mx-4" >
                     {
                     displayNotes.map((item) => (
-                        <li key = {item.id}>
-                            <h6>{item.title}</h6>
-                            <p>{item.content}</p>
-                            <button onClick = {() => handleDelete(item.id)}>Del</button>
+                        <li key = {item.id} className = "border rounded p-4 hover:bg-purple-100">
+                            <div className = "flex flex-row justify-between mb-2">
+                                <h6 className = "underline decoration-1 underline-offset-2 font-semibold font-mono">{item.title}</h6>
+                                <button onClick = {() => handleDelete(item.id)} className = "border px-1 text-sm hover:bg-red-400 hover:text-white hover:border-black">Delete</button>
+                            </div>
+                            <p className = "font-light font-serif">{item.content}</p>
+                            
                         </li>
                     ))
                     }
