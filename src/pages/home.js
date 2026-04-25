8ea4d81868d9f26d759203090cb29d6c9f081a64
Shiwaangee <shiwaangee@gmail.com>
2026-04-25 13:30:39 +0530

    style updated

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,10 +6,10 @@ function Home() {
         <div className="bg-gray-100 min-h-screen ">
             <NavBar />
             <div className="flex flex-col items-center min-h-screen justify-center ">
-                <h1 className="text-gray-800">Welcome {currentUser ? `, ${currentUser}` : ""}</h1>
-                <p className="text-gray-600">
+                <h1 className="text-gray-800 text-3xl mb-2">Welcome {currentUser ? `, ${currentUser}` : ""}</h1>
+                <p className="text-gray-600 text-lg">
                     You can manage your tasks, create notes, and always come back to
-                    continue. If you want a guide to understand how to use the site,
+                    continue.If you want a guide to understand how to use the site,
                     please go to the Help section.
                 </p>
             </div>
